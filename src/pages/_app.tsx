97e70723d4942b6fad7e54a9bfdd4ed98bced300
carneiroRomulo <romulo.carneirodk@gmail.com>
2020-12-16 16:50:54 -0300

    ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import GlobalStyle from '../styles/global'
+import { ThemeProvider } from 'styled-components'
+
+import theme from '../styles/theme'
 
 const Fraternity: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    background: '#F0F0F7',
+    primary: '#262626'
+  }
+}
+
+export default theme
