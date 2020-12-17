e16ffc268922d36b9f45b63ba52c0ca88d056768
carneiroRomulo <romulo.carneirodk@gmail.com>
2020-12-17 20:45:24 -0300

    Resizing the content box

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -2,10 +2,18 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 87.5rem;
-  margin-top: 100px;
+
   display: flex;
   justify-content: center;
+
   font-size: 50px;
 
-  border: 5px solid #000000;
+  position: absolute;
+  top: 200px;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+
+  background-color: ${props => props.theme.colors.box};
+  border-radius: 15px;
 `
